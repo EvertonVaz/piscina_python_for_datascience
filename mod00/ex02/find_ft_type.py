ba7858a23bef636d3cc2640696ff2a68600d6ff7
egeraldo <etovaz.web@gmail.com>
2026-03-01 18:40:21 -0300

    Adiciona novos testes no arquivo tester.py para validar a funcionalidade da função count_in_list. Atualiza o arquivo find_ft_type.py para melhorar a formatação do dicionário e adiciona o arquivo .gitignore no diretório mod00 para ignorar o arquivo 0-Starting.pdf.

--- mod00/ex02/find_ft_type.py
+++ mod00/ex02/find_ft_type.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     ft_list = ["Hello", "tata!"]
     ft_tuple = ("Hello", "toto!")
     ft_set = {"Hello", "tutu!"}
-    ft_dict = {"Hello" : "titi!"}
+    ft_dict = {"Hello": "titi!"}
     all_thing_is_obj(ft_list)
     all_thing_is_obj(ft_tuple)
     all_thing_is_obj(ft_set)
